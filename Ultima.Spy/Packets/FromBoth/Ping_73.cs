e4719a2e5f809bcf04c0c834041ec181b2fd7738
extreme <extreme@18b59796-6d1b-4aa0-ac13-1f8970db953b>
2014-11-28 02:26:47 +0000

    New packets added.

---
+++ Ultima.Spy/Packets/FromBoth/Ping_73.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System;
+
+namespace Ultima.Spy.Packets
+{
+	[UltimaPacket( "Ping", UltimaPacketDirection.FromBoth, 0x73 )]
+    public class PingPacket : UltimaPacket
+	{
+        private int _Value;
+        [UltimaPacketProperty]
+        public int Value { get { return _Value; } }
+
+		protected override void Parse( BigEndianReader reader )
+		{
+			reader.ReadByte(); // ID
+
+            _Value = reader.ReadByte();
+		}
+	}
+}
